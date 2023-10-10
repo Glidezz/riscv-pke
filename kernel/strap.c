daa30ae27f6f57e4f8f1c72e4de5b426b3a03cb2
yinzida <1975793885@qq.com>
2023-10-10 22:32:37 +0800

    my work on lab1_3 is done.

--- kernel/strap.c
+++ kernel/strap.c
@@ -33,12 +33,12 @@ static uint64 g_ticks = 0;
 // added @lab1_3
 //
 void handle_mtimer_trap() {
-  sprint("Ticks %d\n", g_ticks);
+  sprint("Ticks %d\n", g_ticks++);
   // TODO (lab1_3): increase g_ticks to record this "tick", and then clear the "SIP"
   // field in sip register.
   // hint: use write_csr to disable the SIP_SSIP bit in sip.
-  panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
-
+  //panic( "lab1_3: increase g_ticks by one, and clear SIP field in sip register.\n" );
+  write_csr(sip, 0);
 }
 
 //
